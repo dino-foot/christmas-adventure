4d179b6e69c9b88d64b4723e2fd37ec41d4c5592
Shohanur Rahaman <shohan4556@gmail.com>
2021-12-09 20:52:56 +0600

    Update Preloader.js

--- src/Preloader.js
+++ src/Preloader.js
@@ -1,33 +1,40 @@
 import snowBall from "../public/sprites/snowball.png";
 import snowFlake from "../public/sprites/snowflake.png";
-import snowManbody from '../public/sprites/body.png'
-import snowManHead from '../public/sprites/head-1.png'
+import snowMan from "../public/sprites/snowman.png";
+import tree1 from "../public/sprites/tree-1.png";
+import tree2 from "../public/sprites/tree-2.png";
+
+// import snowManbody from '../public/sprites/body.png'
+// import snowManHead from '../public/sprites/head-1.png'
 
 export default class Preloader extends Phaser.Scene {
-  constructor() {
-    super("preloader");
-  }
+    constructor() {
+        super("preloader");
+    }
 
-  init() {
-    this.load.on("progress", this.onLoading, this);
-    this.load.on("complete", this.onCompleteLoading, this);
-  }
+    init() {
+        this.load.on("progress", this.onLoading, this);
+        this.load.on("complete", this.onCompleteLoading, this);
+    }
 
-  preload() {
-    this.load.image("snowball", snowBall);
-    this.load.image("snowfake", snowFlake);
-    this.load.image('body', snowManbody)
-    this.load.image('head', snowManHead)
-  }
+    preload() {
+        this.load.image("snowball", snowBall);
+        this.load.image("snowfake", snowFlake);
+        this.load.image("snowman", snowMan);
+        this.load.image("tree1", tree1);
+        this.load.image("tree2", tree2);
+        // this.load.image('body', snowManbody)
+        // this.load.image('head', snowManHead)
+    }
 
-  create() {}
+    create() {}
 
-  onLoading(progress) {
-    console.log("loading assets ", progress);
-  }
+    onLoading(progress) {
+        console.log("loading assets ", progress);
+    }
 
-  onCompleteLoading() {
-    console.log("loading assets done");
-    this.scene.start("game");
-  }
+    onCompleteLoading() {
+        console.log("loading assets done");
+        this.scene.start("game");
+    }
 }
